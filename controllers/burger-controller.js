9d501b4590a22e84b4799ea39fc370d4556c6698
LJFrancis <joe.mengis@gmail.com>
2020-08-22 16:44:27 -0700

    add on click events to public/js/burger.js

--- controllers/burger-controller.js
+++ controllers/burger-controller.js
@@ -34,19 +34,20 @@ router.put("/api/burgers/:id", function(req, res) {
             res.status(200).end();
         }
     });
-});
-
-router.deleteOne(condition, function(req, res) {
-    let condition = "id =" + req.params.id;
-    console.log("condition", condition);
-
-    burger.deleteOne(condition, function(result){
-        if((result, changedRows === 0)) {
-            return res.status(404).end();
-        } else {
-            res.status(200).end();
-        }
+    router.deleteOne(condition, function(req, res) {
+        let condition = "id = " + req.params.id;
+        console.log("condition", condition);
+    
+        burger.deleteOne(condition, function(result){
+            if((result, changedRows === 0)) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        });
     });
 });
+
+
 
 module.exports = router;
