ecf3ca3e98eb791da55433e31e19acba57af3758
LJFrancis <joe.mengis@gmail.com>
2020-08-26 11:13:55 -0700

    add jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,18 @@
 const mysql = require("mysql");
 
-connection = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Singles@220bpm",
-    database: "burgers_db"
-});
+
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Singles@220bpm",
+        database: "burgers_db"
+    });
+};
+
 connection.connect(function(err) {
     if (err) {
         console.error("connection error: " + err.stack);
